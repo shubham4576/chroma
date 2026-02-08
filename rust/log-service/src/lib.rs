62d82a4954d95b1ba23dfed18aa5f31863b90819
Robert Escriva <robert@trychroma.com>
2026-02-08 13:33:47 -0800

    [BUG] do not call try_join_all with zero futures
    I don't know how this could be tripped, but if futures were to become
    zero, the result would be a hang.

--- rust/log-service/src/lib.rs
+++ rust/log-service/src/lib.rs
@@ -2092,10 +2092,15 @@ impl LogServer {
             })
             .collect::<Vec<_>>();
         let try_join_all_span = tracing::info_span!("join all");
-        let record_batches = futures::future::try_join_all(futures)
-            .instrument(try_join_all_span)
-            .await
-            .map_err(|err: Error| Status::new(err.code().into(), err.to_string()))?;
+        let record_batches = if !futures.empty() {
+            futures::future::try_join_all(futures)
+                .instrument(try_join_all_span)
+                .await
+                .map_err(|err: Error| Status::new(err.code().into(), err.to_string()))?
+        } else {
+            tracing::warn!("empty futures");
+            vec![]
+        };
         let mut records = Vec::with_capacity(pull_logs.batch_size as usize);
         let record_batch_iter = tracing::info_span!("record_batch_iter");
         let _guard = record_batch_iter.enter();
